d19e41cf2c7cd43ff4cf099654a34b350036d1da
Linkazen <jpch2k4@gmail.com>
2026-05-18 22:50:30 +0100

    Changed url

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://linkazen.github.io/JH-Portfolio',
+  homepage: 'https://linkazen.dev',
   title: 'JH Portfolio',
 }
 
