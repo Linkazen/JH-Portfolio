e5791b0a8327fbb0763b911726556d2ed41af701
Linkazen <jpch2k4@gmail.com>
2026-05-18 19:34:47 +0100

    Removed comment

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://linkazen.github.io', // Change this.
+  homepage: 'https://linkazen.github.io',
   title: 'JH Portfolio',
 }
 
