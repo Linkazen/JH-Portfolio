04d80e09894cf8793ffa78ae8358439078fa81f3
Linkazen <jpch2k4@gmail.com>
2026-05-18 19:50:51 +0100

    Fixed homepage (again)

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://linkazen.github.io',
+  homepage: 'https://linkazen.github.io/JH-Portfolio',
   title: 'JH Portfolio',
 }
 
